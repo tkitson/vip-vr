178aa043c00ba2c080d8de8ea5cff413ab06b16e
tkitson <tkitson@udel.edu>
2018-10-21 23:40:44 -0400

    started working on giving different players different, consitent colours accross systems

---
+++ project-vaporwave/project-vaporwave-unity/Assets/JonFolder/CustomNetworkManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class CustomNetworkManager : NetworkManager {
+
+	Color[] playerColors = new Color[] {Color.red, Color.blue};
+
+	public override void OnServerAddPlayer(
+		NetworkConnection conn, 
+		short playerControllerId
+	){
+		//print(playerControllerId);
+		GameObject player = (GameObject)Instantiate(playerPrefab, Vector3.zero, Quaternion.identity);
+        player.GetComponent<Player>().setColor(playerColors[playerControllerId]);
+        NetworkServer.AddPlayerForConnection(conn, player, playerControllerId);
+	}
+}
---
+++ project-vaporwave/project-vaporwave-unity/Assets/JonFolder/Player.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class Player : NetworkBehaviour
+{
+    [SyncVar]
+    public Color color;
+
+    public void setColor(Color color) {
+        this.color = color;
+
+        //find the renderer for this Player
+        Renderer rend = GetComponent<Renderer>();
+
+        //find the shader for this Player, and adjust its color
+        //to match the given color
+        rend.material.shader = Shader.Find("_Color");
+        rend.material.SetColor("_Color", Color.red);
+    }
+
+    public override void OnStartLocalPlayer()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, z);
+    }
+}
